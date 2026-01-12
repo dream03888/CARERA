514dca2dfa8e183bcd5b0449a223b9328f1bc7a0
ESAS <teerapongchanon@gmail.com>
2026-01-12 17:18:05 +0700

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,19 +1,12 @@
 export const environment = {
-
-//  socketEndpoint: 'ws://10.0.0.100:9876',
-    // socketEndpoint: 'http://10.0.0.100:5634',
-        socketEndpoint: 'https://apis-camera-production.up.railway.app',
-
-
-
+  //  socketEndpoint: 'ws://10.0.0.100:9876',
+  // socketEndpoint: 'http://10.0.0.100:5634',
+  socketEndpoint: 'https://apis-camera-production.up.railway.app',
 
   appVersion: '2.0.0',
   AUTH_KEY: 'auth-token',
   USER_KEY: 'user-data',
   ROUTE_KEY: 'route-path',
   VALIDATE_KEY: 'validate',
-  PROGRAM_ID: 1
+  PROGRAM_ID: 1,
 };
-
-
-
