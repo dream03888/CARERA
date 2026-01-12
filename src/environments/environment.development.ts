4458258e8b8076ac85f8a1397d2451c69f303ae8
ESAS <teerapongchanon@gmail.com>
2026-01-12 15:47:39 +0700

    update env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,9 @@
 export const environment = {
 
 //  socketEndpoint: 'ws://10.0.0.100:9876',
-    socketEndpoint: 'http://10.0.0.100:5634',
+    // socketEndpoint: 'http://10.0.0.100:5634',
+        socketEndpoint: 'https://apis-camera-production.up.railway.app',
+
 
 
 
